14cd607952eef1bf24ad1b5d95b8b4b8034495dd
BaturinIgor <baturin29082000@mail.ru>
2020-07-13 13:55:51 +0300

    add trade point choice

---
+++ trade_point_choice/src/main/java/trade_point_choice/entity/TradePoint.java
@@ -0,0 +1,29 @@
+package trade_point.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import javax.persistence.*;
+
+@Entity
+@Table(name = "TradePoint")
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class TradePoint {
+
+    @Id
+    @Column(name = "ID", nullable = false, unique = true)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "address", nullable = false)
+    private String address;
+
+    @Column(name = "latitude", unique = true, nullable = false)
+    private Double latitude;
+
+    @Column(name = "longitude", unique = true, nullable = false)
+    private Double longitude;
+}
