d8678bf5992bc064eb33d64ed717f24b48e6b126
KoT9R <vandal5310@gmail.com>
2020-07-17 16:11:22 +0500

    Fix return JSON getNearesPickPoint

--- src/main/java/trade_point/controller/TradePointController.java
+++ src/main/java/trade_point/controller/TradePointController.java
@@ -52,7 +52,6 @@ public class TradePointController {
             value = "Get nearest pick point",
             authorizations = @Authorization(value="jwtToken")
     )
-    @JsonView(Views.Compact.class)
     public ResponseEntity<Object> getNearestPickPoint(@RequestParam Double latitude, @RequestParam Double longitude ) {
         if (latitude == null || longitude == null) {
             return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
